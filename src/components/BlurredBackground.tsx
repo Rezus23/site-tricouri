8177d77075128db0f0e7da83850f178def3c7100
Rezus23 <catalin_ionut.rezus@stud.etti.upb.ro>
2025-11-28 01:01:11 +0200

    regelewc

--- src/components/BlurredBackground.tsx
+++ src/components/BlurredBackground.tsx
@@ -3,7 +3,7 @@ export default function BlurredBackground() {
     <div 
       className="fixed inset-0 z-[-1] bg-cover bg-center bg-no-repeat"
       style={{ 
-        backgroundImage: "url('/images/unnamed.png')",
+        backgroundImage: "url('/images/wc-bg.png')",
         // Aici controlăm cât de tare este blurată imaginea (blur-md, blur-lg, blur-xl)
         filter: "blur(8px) brightness(0.7)" 
       }}
