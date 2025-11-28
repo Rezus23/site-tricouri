f3594b958531a00cb38b100510579b36fd2bd4fa
Rezus23 <catalin_ionut.rezus@stud.etti.upb.ro>
2025-11-28 17:51:40 +0200

    25

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,15 @@ import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
 import { CartProvider } from "@/context/CartContext";
 import { AuthProvider } from "@/context/AuthContext"; 
+import Navbar from "@/components/Navbar";
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <CartProvider>
         <Layout>
+          <Navbar />
           <Component {...pageProps} />
         </Layout>
       </CartProvider>
