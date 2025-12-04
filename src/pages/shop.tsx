94a5f084e3ff8014d1fdc745e5442515f487f696
Rezus23 <catalin_ionut.rezus@stud.etti.upb.ro>
2025-12-04 23:53:37 +0200

    da

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -32,7 +32,7 @@ export default function Shop() {
     { id: "tricouri", label: "Sezon 24/25" },
     { id: "retro", label: "Retro" },
     { id: "nationale", label: "Echipe Naționale" },
-    { id: "sorturi", label: "Șorturi" },
+    { id: "sorturi", label: "Custom" },
   ];
 
   useEffect(() => {
