080d9ef765ca35d740cc901e45b0deafc2ba0ee1
Rezus23 <catalin_ionut.rezus@stud.etti.upb.ro>
2025-12-10 20:04:42 +0200

    Da

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -127,7 +127,7 @@ export default function Shop() {
 
       <div className="max-w-[1400px] mx-auto p-6 relative z-10 pt-10">
         <h1 className="text-5xl md:text-7xl font-black text-center mb-8 text-white tracking-tighter drop-shadow-xl uppercase">
-          Magazin <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-600">Oficial</span>
+          Descoperirea pasiunii tale începe <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-600">AICI</span>
         </h1>
 
         {/* DROPDOWN MENU */}
