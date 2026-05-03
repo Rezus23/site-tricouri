7b0de78e5b059de4cc54ffdce7742b8d382e95f8
Rezus23 <catalin_ionut.rezus@stud.etti.upb.ro>
2026-05-03 20:01:30 +0300

    AD

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -177,7 +177,7 @@ export default function Home() {
 
                     FEEL THE <br />
 
-                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-golden-400 via-white to-blue-400 animate-gradient bg-300%">
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-white to-blue-400 animate-gradient bg-300%">
 
                         WORLD CUP.
 
@@ -215,9 +215,9 @@ export default function Home() {
 
                             <p className="text-xs md:text-sm font-bold text-gray-200 uppercase tracking-widest leading-relaxed">
 
-                                PENTRU COMENZI DE PESTE <span className="text-golden-400 text-base mx-1 bg-white/10 px-2 rounded border border-blue-400/30">2 PRODUSE </span>
+                                PENTRU COMENZI DE PESTE <span className="text-yellow-400 text-base mx-1 bg-white/10 px-2 rounded border border-blue-400/30">2 PRODUSE </span>
 
-                                AI TRANSPORTUL <span className="text-golden-400 underline decoration-blue-400 underline-offset-4">GRATUIT</span> <br className="hidden md:block"/>
+                                AI TRANSPORTUL <span className="text-yellow-400 underline decoration-blue-400 underline-offset-4">GRATUIT</span> <br className="hidden md:block"/>
 
 
                             </p>
